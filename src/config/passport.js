90a29f7f7e19bfcf736ff111d5a3e214f3134955
navjotmaan <navjotmaan137@gmail.com>
2026-03-19 07:23:44 +0000

    add usernameField in local strategy to look for email

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,31 +3,34 @@ const LocalStrategy = require('passport-local').Strategy;
 const pool = require('../db/pool');
 const bcrypt = require("bcryptjs");
 
-passport.use(new LocalStrategy(async (username, password, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-    const user = rows[0];
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  }, 
+  async (email, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+      const user = rows[0];
 
-    if (!user) return done(null, false, { message: "Incorrect username" });
-    
-    const match = bcrypt.compare(password, user.password);
-    if (!match) return done(null, false, { message: "Incorrect password" });
+      if (!user) return done(null, false, { message: "Incorrect username" });
+      
+      const match = bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { message: "Incorrect password" });
 
-    return done(null, user);
-  } catch(err) {
-    return done(err);
-  }
-}));
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  }));
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    done(null, rows[0]);
-  } catch(err) {
-    done(err);
-  }
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+      done(null, rows[0]);
+    } catch(err) {
+      done(err);
+    }
 });
