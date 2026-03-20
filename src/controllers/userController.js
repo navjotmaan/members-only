10bc3f7482875402339ff3f77f1237cc2541218b
navjotmaan <navjotmaan137@gmail.com>
2026-03-20 05:56:29 +0000

    add functions to check login info and to logout user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const db = require('../db/queries');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const { validationResult } = require('express-validator');
 
 const showSignUpForm = (req, res) => {
@@ -9,7 +10,10 @@ const showSignUpForm = (req, res) => {
 async function createUser(req, res) { 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).render('sign-up', { errors: errors.array() });
+        return res.status(400).render('sign-up', { 
+            errors: errors.array(),
+            oldInput: req.body
+        });
     }
 
     try {
@@ -39,10 +43,36 @@ const logoutUser = (req, res, next) => {
   });
 };
 
-function ensureAuthenticated(req, res, next) {
+const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) return next();
   res.redirect('/login');
 }
+
+const checkLoginInfo = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('login', { 
+      errors: errors.array(),
+      oldInput: req.body
+    });
+  }
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err); 
+
+    if (!user) {
+      return res.render('login', { 
+        authError: info.message,
+        oldInput: req.body 
+      });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect('/dashboard');
+    });
+  })(req, res, next); 
+};
 
 const showHomePage = (req, res) => {
     if (req.isAuthenticated()) {
@@ -58,4 +88,5 @@ module.exports = {
     showHomePage,
     showSignUpForm,
     ensureAuthenticated,
+    checkLoginInfo
 };
