bd96f137d5058f5da68d0a7e0861fca8fc396bd9
navjotmaan <navjotmaan137@gmail.com>
2026-03-20 05:55:08 +0000

    clean up routes by removing logout and authentication functions and instead passing them as middlewares

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const userRouter = Router();
-const passport = require('passport');
+const validateUser = require('../validators/userValidator');
 
 const userController = require('../controllers/userController');
 const messageController = require('../controllers/messageController');
@@ -8,23 +8,12 @@ const messageController = require('../controllers/messageController');
 userRouter.get('/', userController.showHomePage);
 
 userRouter.get('/sign-up', userController.showSignUpForm);
-userRouter.post('/sign-up', userController.createUser);
+userRouter.post('/sign-up', validateUser.validateSignUp, userController.createUser);
+
 userRouter.get('/login', userController.showLogInForm);
+userRouter.post('/login', validateUser.validateLogin, userController.checkLoginInfo);
 
-userRouter.post('/login', passport.authenticate('local', {
-  successRedirect: '/dashboard',
-  failureRedirect: '/login'
-}));
-
-userRouter.get("/logout", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
+userRouter.get('/logout', userController.logoutUser);
 userRouter.get('/dashboard', userController.ensureAuthenticated, messageController.getAllMessages);
 
 module.exports = userRouter;
