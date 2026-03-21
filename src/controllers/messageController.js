98b1ce02df682f05edcd4e8656258b67c3c037ac
navjotmaan <navjotmaan137@gmail.com>
2026-03-21 10:17:14 +0000

    add validation result to check errors

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,15 +1,24 @@
 const db = require('../db/queries');
+const { validationResult } = require('express-validator');
 
 const showMessageForm = (req, res) => {
   res.render('message');
 };
 
 async function createMessage(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).render('message', { 
+        errors: errors.array(),
+        oldInput: req.body
+        });
+    }
+
     try {
         const { title, message } = req.body;
 
         await db.createMessage(req.user.id, title, message);
-        res.status(201).json('message created');
+        res.redirect('/dashboard');
     } catch (err) {
         console.log(err);
         res.status(500).send('Error creating message');
