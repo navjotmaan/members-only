167c8759686865a0ac2f3e5945bf370b137cc9db
navjotmaan <navjotmaan137@gmail.com>
2026-03-21 16:54:05 +0000

    change variable name from validateUser to validator

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const messageRouter = Router();
-const validateUser = require('../validators/userValidator');
+const validator = require('../validators/userValidator');
 
 const messageController = require('../controllers/messageController');
 
 messageRouter.get('/form', messageController.showMessageForm);
-messageRouter.post('/add', validateUser.validateMessage, messageController.createMessage);
+messageRouter.post('/add', validator.validateMessage, messageController.createMessage);
 
 module.exports = messageRouter;
